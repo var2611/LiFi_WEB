3dea2ceee98104e1a238c6e653c30c9386184e55
var2611 <root123#>
2021-05-28 13:37:46 +0530

    LiFi Attendance : Web

--- resources/views/hr_dashboard.blade.php
+++ resources/views/hr_dashboard.blade.php
@@ -6,10 +6,12 @@
         <a><i class="fas fa-home"></i>Dashboard</a>
     </div>
     <div class="card-list">
-        <div class="card-hr">
-            <i class="fas fa-users fa-2x"></i>
-            <h1>Attendance</h1>
-        </div>
+        <a href="{{ route('UsersAtt') }}">
+            <div class="card-hr">
+                <i class="fas fa-users fa-2x"></i>
+                <h1>Attendance</h1>
+            </div>
+        </a>
         <a href="{{ route('UsersList') }}">
             <div class="card-hr">
                 <i class="fas fa-user fa-2x"></i>
