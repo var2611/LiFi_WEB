79b3a57997d482c8c60f9aa3068dde5d8dbce730
var2611 <root123#>
2021-06-26 02:04:57 +0530

    LiFi Attendance : WEB HRMS Leave Plugin install

--- config/app.php
+++ config/app.php
@@ -169,6 +169,7 @@ return [
         Laravel\Passport\PassportServiceProvider::class,
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
         LaravelViews\LaravelViewsServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -230,7 +231,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
     ],
 
 ];
